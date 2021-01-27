c7298981966f25be6b3c5ad53ba1f50b326a8d8d
GuteRunde <ghy1100ghy@gmail.com>
2021-01-27 11:55:31 +0000

    first commit

---
+++ CanUFind/Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public float mousespeed;
+
+    public Transform player;
+
+    private float xmove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y;
+        x = Input.GetAxis("Mouse X") * mousespeed * Time.deltaTime;
+        y = Input.GetAxis("Mouse Y") * mousespeed * Time.deltaTime;
+        xmove = xmove - y;
+        xmove = Mathf.Clamp(xmove, -90, 90);
+
+        this.transform.localRotation = Quaternion.Euler(xmove,0,0);
+
+        player.Rotate(Vector3.up*x);
+    }
+}
---
+++ CanUFind/Assets/Scripts/Playermove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+
+    private float speed = 10;
+
+    public CharacterController playercontroller;
+
+    private float jumpspeed = 5;
+
+    private float g = 20;
+
+    Vector3 move;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x=0, z=0;
+        if (playercontroller.isGrounded)
+        {
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+
+            move =( transform.right * x + transform.forward * z)*speed;
+
+            if (Input.GetAxis("Jump")==1)
+            {
+                move.y = jumpspeed;
+            }
+        }
+        move.y = move.y - g * Time.deltaTime;
+        
+        playercontroller.Move(move*Time.deltaTime);
+
+    }
+}
